ff012751e2448bdc0f23aaf41343fa8008a95b99
Rosana <rcathayde2012@gmail.com>
2022-11-07 17:49:10 -0300

    codando

---
+++ intro-react/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Page from './components/Page'
+
+function App() {
+  return (
+    <div className="App"><Page/></div>
+  );
+}
+export default App;
---
+++ intro-react/src/components/Page/index.js
@@ -0,0 +1,17 @@
+import './style.css'
+function Page() {
+    return (
+        <div className="pagina">
+            <h1>Say Hello to</h1> 
+            <h1>ReactJS</h1>
+            <p>You will learn how to use</p> 
+            <p>the most popular frontend library,</p>
+            <p>and become a super Ninja developer.</p>
+            <img scr=".scr/images/icon1.png" alt=""></img>
+            <img scr=".scr/images/icon2.png" alt=""></img>
+            <img scr=".scr/images/icon2.png" alt=""></img>
+            <img scr=".scr/images/icon2.png" alt=""></img>    
+        </div>
+    ) 
+} 
+export default Page;
